50394c21e8149129fb005481f29879941efad1d2
Akhil-Titus <akhiltitusvalooran@gmail.com>
2023-07-12 05:51:07 +0530

    first push

---
+++ todo_app/urls.py
@@ -0,0 +1,17 @@
+from . import views
+from django.urls import path
+
+app_name = 'todo_app'
+
+urlpatterns = [
+
+    path('', views.add, name='home'),
+    # path('details/', views.details, name='details'),
+    path('delete/<int:task_id>/', views.delete, name='delete'),
+    path('update/<int:id>/', views.update, name='update'),
+    # path('delete/<int:id>/', views.delete, name='delete'),
+    path('cbvhome/', views.TaskListView.as_view(), name='cbvhome'),
+    path('cbvdetail/<int:pk>', views.TaskDetailView.as_view(), name='cbvdetail'),
+    path('cbvupdate/<int:pk>', views.TaskUpdateView.as_view(), name='cbvupdate'),
+    path('cbvdelete/<int:pk>', views.TaskDeleteView.as_view(), name='cbvdelete'),
+]
